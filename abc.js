19f450be1f94e2d1d721114666d55a4134530b64
raza ayaz <razaayaz@yahoo.com>
2024-12-27 01:34:38 +0500

    Add files via upload

---
+++ abc.js
@@ -0,0 +1,8 @@
+const a = 4;
+const b = 1;
+const c = b+1;
+const d = a * c;
+
+document.write(a + 'X' + b + '=' + a)
+document.write(a + 'X' + c + '=' + d)
+document.write(a + 'X' + c+ '=' + d)
---
+++ app.js
@@ -0,0 +1,26 @@
+// let x = 5;
+// let y = 6;
+// let z = x+y;
+// console.log(x);
+// console.log(++x);
+// console.log(x++);
+// console.log(x);
+// console.log((x+y)-1);
+// console.log(z);
+// console.log(x-y);
+// console.log(x--);
+// console.log(--x);
+
+
+var t = 600;
+
+var total = t * 5;
+document.write( 'Total cost to buy 5 ticket is ' + total + 'PKR');
+
+
+
+
+
+
+
+
